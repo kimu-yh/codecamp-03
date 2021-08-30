d7f2a700d2e72bc08c2022647e7ddc63a22608a8
kclaireyh <kclaireyh@gmail.com>
2021-08-31 02:02:47 +0900

    programmers 1 to 15 문제 풀이

---
+++ programmers/programmars.js
@@ -0,0 +1,53 @@
+//연습문제
+//1.
+let name = '김여현';
+
+//2.
+name = '철수';
+
+//3.
+fruits = ["사과", "바나나", "파인애플"]
+
+//4.
+let lastIndex = fruits.length - 1;
+newFruits.push(fruit[lastIndex]);
+
+//5.
+let newArray = students.slice(0, 3);
+
+//6.
+fruits = fruits.map(x => "맛있는 " + x);
+
+//7. 
+const number = "01012345678";
+
+let head = number.slice(0, 3);
+let body = number.slice(3, 7);
+let tail = number.slice(7);
+
+let cellPhoneNumber = [head, body, tail];
+
+//8. 
+student.name = "철수";
+
+//9. 
+student.school = school;
+
+//10.
+delete student.drink;
+
+//11.
+classmates[1].school = "다람쥐초등학교";
+
+//12. 문제가 없음
+
+//13.
+typeof str 
+typeof number 
+
+//14.
+Array.isArray(array)
+Array.isArray(object)
+
+//15.
+str = String(num);
