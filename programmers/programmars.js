b457e0a7d8f3dbcf038056a47e60176ae86fc84c
kclaireyh <kclaireyh@gmail.com>
2021-09-02 19:59:28 +0900

    Add: 2021.09.03일차 programmars 문제풀이 업데이트

--- programmers/programmars.js
+++ programmers/programmars.js
@@ -272,3 +272,81 @@ let makeOdd = (num) => {
 	}
 	return result
 }
+
+//반복문: 가장 큰 수 찾기 27.
+function bigNum(str) {
+	let biggest = 0;
+  for (let i = 0; i < str.length; i++) {
+		if (Number(str[i]) >= biggest) {
+		biggest = Number(str[i]);
+	  } 
+	}
+return biggest;
+}
+
+//조건문: 점수에 따른 등급 41.
+function grade(score) {
+	let result = "";
+  if (score < 0 || score > 100 ) {
+		result = "잘못된 점수입니다"
+	} else if (score >= 90) {
+		result = "A"
+	} else if (score >= 80) {
+		result = "B"
+	} else if (score >= 70) {
+		result = "C"
+	} else if (score >= 60) {
+		result = "D"
+	} else {
+		result = "F"
+	}
+	return result
+}
+
+//반복문: 마이페이지 43.
+const myShopping = [
+	{ category: "과일", price: 12000　},
+	{ category: "의류", price:10000　 },
+	{ category: "의류", price: 20000　},
+	{ category: "장난감", price: 9000 },
+	{ category: "과일", price: 5000　 },
+	{ category: "의류", price: 10000  },
+	{ category: "과일", price: 8000　　},
+	{ category: "의류", price: 7000　　},
+	{ category: "장난감", price: 5000  },
+	{ category: "의류", price: 10000　 },
+]
+/*  해당 목록에서 "의류"를 구매한 횟수와 총 금액을 나타내고, 
+		"의류"를 구매한 횟수에 따라 등급을 나타내세요.
+
+		등급표
+		"0~2"  ⇒ Bronze
+		"3~4" ⇒ Silver
+		5이상 ⇒ Gold 
+*/
+
+// 구매한 횟수를 담아주는 변수
+// 구매한 총 금액을 담아주는 변수
+// 구매한 횟수에 따라 결정되는 등급을 담아주는 변수
+let count = 0;
+let price = 0;
+let grade = "";
+let result = '';
+
+for(let i = 0; i < myShopping.length; i = i + 1) {
+	if(myShopping[i].category === "의류") {
+		count = count + 1;
+		price = price + myShopping[i].price;
+
+		if(count >= 0 && count <= 2) {
+			grade = "Bronze";
+		} else if(count >= 3 && count <= 4) {
+			grade = "Sliver";
+		} else if(count >= 5) {
+			grade = "Gold";
+		}
+	}
+	result = `의류를 구매한 횟수는 총 ${count}회 금액은 ${price}원이며 등급은 ${grade}입니다.`
+	console.log(result)
+	return result
+}
