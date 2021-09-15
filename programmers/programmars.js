f9fb73c9230cbac6c59d80277a1df3c0d6cbcbc5
kclaireyh <kimu.yohyon@gmail.com>
2021-09-15 10:03:17 +0900

    Add: 13일차 프로그래머스 풀이 업데이트

--- programmers/programmars.js
+++ programmers/programmars.js
@@ -581,4 +581,15 @@ function solution(arr, divisor) {
 	return answer.sort((a, b) => a - b)
 }
 
-
+//13일차 콜라츠 추측
+function solution(num) {
+	let i = 0
+			while(i < 500) {
+					if (num === 1) return i;
+					num = num % 2 === 0 
+					? num/2
+					: num*3 + 1
+					i++;
+			}
+	return -1
+}
