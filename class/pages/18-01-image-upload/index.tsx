1daed9fa04b7dba9cebca9c578692aae9d8bf346
kclaireyh <kimu.yohyon@gmail.com>
2021-09-28 15:06:57 +0900

    Add: 18일차 Class: 사진 넣기  Update

---
+++ class/pages/18-01-image-upload/index.tsx
@@ -0,0 +1,108 @@
+import { gql, useMutation } from "@apollo/client";
+import { useState, useRef } from "react";
+
+
+const UPLOAD_FILE = gql`
+  mutation uploadFile($file: Upload!) {
+    uploadFile(file: $file) {
+      url
+    }
+  }
+`;
+
+const CREATE_BOARD = gql`
+  mutation createBoard($createBoardInput: CreateBoardInput!) {
+    createBoard(createBoardInput: $createBoardInput) {
+      _id
+    }
+  }
+`
+
+export default function ImageUploadPage() {
+  const [imageUrls, setImageUrls] = useState(['']);
+  const [createBoard] = useMutation(CREATE_BOARD)
+  const [uploadFile] = useMutation(UPLOAD_FILE);
+  const fileRef = useRef<HTMLInputElement>()
+  const [myInputs, setMyInputs] =  useState({
+    writer: '',
+    password: '',
+    title: '',
+    contents: ''
+  })
+
+  const onChangeFile = async (e) => {
+    const myFile = e.target.files[0];
+    console.log(myFile);
+
+    if (!myFile) {
+      alert("파일이 없습니다!");
+      return;
+    }
+
+    if (myFile.size > 5 * 1024 * 1024) {
+      alert("파일 용량이 너무 큽니다. (제한: 5MB)");
+      return;
+    }
+
+    if (!myFile.type.includes("jpeg") && !myFile.type.includes("png")) {
+      alert("jpeg 또는 png만 업로드 가능합니다.");
+      return;
+    }
+
+    const result = await uploadFile({
+      variables: {
+        file: myFile,
+      },
+    });
+    console.log(result.data.uploadFile.url);
+    setImageUrls([result.data.uploadFile.url]);
+  };
+
+  function onClickDiv() {
+    fileRef.current?.click()
+  }
+
+  function onChangeMyInputs(e) {
+    setMyInputs({...myInputs, [e.target.name]: e.target.value})
+  }
+
+  function onClickSubmit() {
+    createBoard({
+      variables: {
+        createBoardInput: {
+          ...myInputs,
+          images: [...imageUrls]
+        }
+      }
+    })
+  }
+
+  return (
+    <>
+      <div>이미지 업로드!!!</div>
+      <div 
+      style={{
+        width: '200px', 
+        height: '50px', 
+        backgroundColor: 'gray',
+        margin: '20px'
+      }}
+      onClick={onClickDiv}
+      >이미지 선택!!!</div>
+      <input 
+      style={{display: "none", margin: '20px'}} 
+      type="file" 
+      onChange={onChangeFile}
+      ref={fileRef} />
+      <img 
+      src={`https://storage.googleapis.com/${imageUrls[0]}`}
+      style={{margin: '20px'}}
+      /><br />
+      작성자: <input type="text" name="writer" onChange={onChangeMyInputs} /><br />
+      비밀번호: <input type="password" name="password"onChange={onChangeMyInputs} /><br />
+      제목: <input type="text" name="title" onChange={onChangeMyInputs} /><br />
+      내용: <input type="text" name="contents" onChange={onChangeMyInputs} /><br />
+      <button onClick={onClickSubmit}>등록하기!</button>
+    </>
+  );
+}
--- class/pages/_app.js
+++ class/pages/_app.js
@@ -1,9 +1,10 @@
 import Layout from "../src/component/commons/layout";
 import "antd/dist/antd.css";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache, ApolloLink } from "@apollo/client";
 import { Global } from "@emotion/react";
 import { globalStyles } from "../src/commons/styles/globalStyles";
 import { initializeApp } from "firebase/app";
+import { createUploadLink } from "apollo-upload-client"
 
 export const firebaseApp = initializeApp({
   apiKey: "AIzaSyAPP0Kp1JEf5xGUWdALUsMrgnLYAijHjlo",
@@ -16,8 +17,13 @@ export const firebaseApp = initializeApp({
 });
 
 function MyApp({ Component, pageProps }) {
+
+  const uploadLink = createUploadLink({
+    uri: "http://backend02.codebootcamp.co.kr/graphql"
+  })
+
   const client = new ApolloClient({
-    uri: "http://backend03.codebootcamp.co.kr/graphql",
+    link: ApolloLink.from([uploadLink]),
     cache: new InMemoryCache(),
   });
 
