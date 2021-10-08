c6198bd451fbb1d93af3da32a4a03d19e07075da
kclaireyh <kimu.yohyon@gmail.com>
2021-10-08 11:04:29 +0900

    add: 27일차 알고리즘 세준샘 풀이 업데이트

--- programmers/programmars.js
+++ programmers/programmars.js
@@ -1086,3 +1086,43 @@ function solution(n, lost, reserve) {
  }
 	return n - lost.length
 }
+//세준샘
+function solution(n, lost, reserve) {
+ 
+	const losted = [...lost]
+	lost = lost.filter((e) => !reserve.includes(e)).sort((a, b) => a - b)
+	reserve = reserve.filter(e => !losted.includes(e)).sort((a, b) => a - b)
+	let answer = n - lost.length
+	for (let i =  0; i < lost.length; i++) {
+			const student =  lost[i]
+			if ( reserve.includes(student - 1)) {
+					reserve.splice(reserve.indexOf(student-1), 1)
+					answer++
+			} else if (reserve.includes( student + 1)) {
+					reserve.splice(reserve.indexOf(student+1), 1)
+					answer++
+			} 
+	}
+		 return answer
+ }
+ //세준샘 메서드풀이
+ function solution(n, lost, reserve) {
+ 
+	const losted = [...lost]
+	lost = lost.filter((e) => !reserve.includes(e)).sort((a, b) => a - b)
+	reserve = reserve.filter(e => !losted.includes(e)).sort((a, b) => a - b)
+	let answer = n - lost.length
+	
+	lost.forEach(s => {
+			const prev = reserve.indexOf(s - 1)
+			const next = reserve.indexOf(s + 1)
+			if (prev !== -1) {
+					reserve.splice(prev, 1)
+					answer++
+			} else if (next !== -1) {
+					reserve.splice(next, 1)
+					answer++
+			}
+	})
+		 return answer
+ }
