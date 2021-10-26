d4d6471ece038c540d087a90949b3adcfcddbf63
kclaireyh <kimu.yohyon@gmail.com>
2021-10-26 11:52:32 +0900

    10.26

--- class-build/pages/boards/index.js
+++ class-build/pages/boards/index.js
@@ -1,6 +1,15 @@
+import Head from "next/head"
+
 export default function BoardsPage() {
 
   return (
+    <>
+    <Head>
+      <meta property="og:title" content="나의 사이트 게시판 페이지"/>
+      <meta property="og:description" content="환영합니다" />
+      <meta property="og:image" content="https://mblogthumb-phinf.pstatic.net/20141112_55/mtvstudy_14157808754648xRHs_PNG/%BC%F6%B4%C9_%BD%AC%B4%C2%BD%C3%B0%A3.PNG?type=w2" />
+    </Head>
     <div>게시판입니다</div>
+    </>
   )
 }
